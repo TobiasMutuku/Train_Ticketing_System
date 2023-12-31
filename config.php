9954d9b58b6a45efc70b171f92a154d9e66332aa
Tobias Mutuku <113021484+TobiasMutuku@users.noreply.github.com>
2023-12-31 14:05:40 +0300

    Add files via upload
    files upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$title = 'HAWK RAILWAYS';
+//E-Ticketing System For Railway
+$supervisor_name = "Ms. Noela Kipyegon";
+$developer_name = "James Karanja Hungu";
+$developer_matric = "16/52HA016";
